4d88c68fb7117a7e9ff91fe342e4531efee52f48
Daniel Loureiro <daniel.loureiro@jumia.com>
2017-09-21 15:30:38 +0100

    Inital commit

---
+++ dbot.py
@@ -0,0 +1,110 @@
+import os
+import time
+from slackclient import SlackClient
+from bs4 import BeautifulSoup
+import requests
+import pickle
+
+# filename for user list
+FILENAME="userlist"
+
+# starterbot's ID as an environment variable
+BOT_ID = 'U6KLQ0W5D'
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+COMMAND_CHANNEL="C6K3HPLQ2" #channel where the bot will serve commands TODO: block to channel
+MASTER="U627R20M1" #IF you want to use a master user for special commands TODO
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient('<bot-key>')
+lastfm_list = {}
+
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+# try to open the file, check for it
+try:
+    lastfm_list = load_obj(FILENAME)
+    pass
+except Exception as e:
+    print ("Filename doesn't exist!")
+    pass
+
+def getSong(username):
+    url = "http://last.fm/user/" + username
+    print(url)
+    result = requests.get(url)
+    soup = BeautifulSoup(result.content, "lxml")
+    current_play = soup.find("tr", "now-scrobbling")
+    if current_play:
+        song = "is listening to: :musical_note: *" + current_play.find("td", "chartlist-name").text.strip().replace("\n", "").replace("\u2014", "-") + "*"
+        return song
+    else:
+        return "*hasn't scrobled anything in a while!*"
+
+# Example of a JSON received by the bot
+# [{'team': 'T625JDJ3V', 'type': 'message', 'channel': 'C6K3HPFQ1', 'user': 'U6S7R20M8', 'text': '.np', 'ts': '1502197658.841748', 'source_team': 'T62AJ6J3V'}]
+
+def handle_command(command, channel, user):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = ""
+    if command.startswith(".np", 0):
+        if user in lastfm_list:
+            print (lastfm_list[user])
+            print("Scrubing...")
+            print(getSong(lastfm_list[user]))
+            response = "<@" + user + "> " + getSong(lastfm_list[user])
+        else:
+            response = "<@" + user +"> to set you last fm user, type: .set <username>"
+    elif command.startswith(".set" , 0):
+        lastfm_list[user] = command.split(" ")[1]
+        save_obj(lastfm_list, FILENAME)
+        response = "<@" + user +"> last fm user set to: " + lastfm_list[user]
+        #response = "Sure...write some more code then I can do that!"
+    else:
+        response = "<@" + user +"> available commands: *.np* and *.set <username>*"
+    if response:
+        slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+def parse_slack_output(slack_rtm_output):
+  """
+      The Slack Real Time Messaging API is an events firehose.
+      this parsing function returns None unless a message is
+      directed at the Bot, based on its ID.
+  """
+  output_list = slack_rtm_output
+  # print(output_list)
+  if output_list and len(output_list) > 0:
+      for output in output_list:
+          if output and 'text' in output and output['text'].startswith('.', 0): # and AT_BOT in output['text']:
+              # print(output)
+              # print(output['text'])
+              # return text after the @ mention, whitespace removed
+              # return output['text'].split(AT_BOT)[1].strip().lower(), \
+              # output['channel']
+              return output['text'], output['channel'], output['user']
+  return None, None, None
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel, user = parse_slack_output(slack_client.rtm_read())
+            if command and channel and user:
+                handle_command(command, channel, user)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
